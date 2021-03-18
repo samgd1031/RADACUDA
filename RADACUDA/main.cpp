531dd110589520797591877d28e12b36b5e4aae7
Sam Dupas <samgd1031@gmail.com>
2021-03-18 04:06:04 -0700

    revised main script to shoot rays from a point on one rectangle and see if they hit another
    TODO:  have rays originate from random points on the surface rather than all from the node directly

--- RADACUDA/main.cpp
+++ RADACUDA/main.cpp
@@ -8,50 +8,97 @@
 
 #include "gnuplot.h"
 
+#include <cstdlib>
+#include <vector>
+#include <string>
+#include <chrono>
+
 
 int main() {
 	
 	// RIGHT NOW THE MAIN FUNCTION BUILDS A RECTANGLE AND CHECKS IF A FEW RAYS HIT IT
 	// 	   NO FANCY CALCULATIONS AT THE MOMENT
-	// rectangle setup
-	point3 r_or = point3(-1.0, -1.0, 1.0);
-	double dim1 = 2;
-	double dim2 = 2;
-	vec3 S1 = vec3(1.0, 0.0, 0.5);
+ 
+	// start timer
+	auto tstart = std::chrono::high_resolution_clock::now();
+
+	// origin rectangle setup
+	point3 r_or = point3(0.0, 0.0, 0.0);
+	double dim1 = 1;
+	double dim2 = 1;
+	vec3 S1 = vec3(1.0, 0.0, 0.0);
 	vec3 S2 = vec3(0.0, 1.0, 0.0);
 
-	rectangle rect = rectangle(r_or, S1, S2, dim1, dim2, 1, 1);
+	rectangle orig_rect = rectangle(r_or, S1, S2, dim1, dim2, 1, 1);
 
-	// ray setup
-	ray R1 = ray(point3(0.0, 0.0, 0.0), vec3(0.0, 0.0, 1.0)); // this ray should hit
-	ray R2 = ray(point3(0.2869, -0.7362, 0.5573), vec3(-0.2295, 0.6767, 0.6996)); // this should also hit
-	ray R3 = ray(point3(-0.7049, -0.6884, 0.2164), vec3(0.7458, 0.5867, -0.3154)); // this ray should miss
+	// target rectangle setup
+    r_or = point3(0.0, 0.0, 1.0);
+	dim1 = 1;
+	dim2 = 1;
+	S1 = vec3(1.0, 0.0, 0.0);
+	S2 = vec3(0.0, 1.0, 0.0);
 
+	rectangle tgt_rect = rectangle(r_or, S1, S2, dim1, dim2, 1, 1);
 
-	hit_record rec1, rec2, rec3;
-
-	bool hit1 = rect.hit(R1, 0, infinity, rec1);
-	std::cout << "Ray 1: " << hit1 << '\n';
-	bool hit2 = rect.hit(R2, 0, infinity, rec2);
-	std::cout << "Ray 2: " << hit2 << '\n';
-	bool hit3 = rect.hit(R3, 0, infinity, rec3);
-	std::cout << "Ray 3: " << hit3 << '\n';
-
-	std::cout << R2.gnuplot_repr(0, (hit2) ? rec2.t : 1) << '\n';
-
+	// plot the rectangles
 	GnuplotPipe gp;
 	gp.sendLine("set hidden3d");
 	gp.sendLine("set view equal xyz");
 	gp.sendLine("set key off");
-	gp.sendLine("set xrange [-1:1]");
-	gp.sendLine("set yrange [-1:1]");
+	gp.sendLine("set xrange [0:1]");
+	gp.sendLine("set yrange [0:1]");
+	gp.sendLine("set zrange [0:1]");
+	gp.sendLine("set cbrange [0:1]");
 	gp.sendLine("unset colorbox");
 	gp.sendLine("set parametric");
-	gp.sendLine("splot " + R1.gnuplot_repr(0,1));
+	gp.sendLine("splot " + orig_rect.gnuplot_repr() + " with pm3d fs solid fc '#A00000FF'");
+	gp.sendLine("replot " + tgt_rect.gnuplot_repr() + " with pm3d fs solid fc '#A0FF0000'");
+
+
+
+	// ray setup
+	int N_rays = 100; // number of rays to shoot from target
 	
-	gp.sendLine("replot " + R2.gnuplot_repr(0,1));
-	gp.sendLine("replot " + R3.gnuplot_repr(0,0.1));	
-	gp.sendLine("replot " + rect.gnuplot_repr());
+	point3 ray_origin = orig_rect.node_list[0].location;
+	std::vector<ray> ray_list;
+	std::vector<hit_record> hit_rec_list;
+	int hit_count = 0;
+
+	ray_list.reserve(N_rays);
+	hit_rec_list.reserve(N_rays);
+
+	// shoot the rays
+	for (int ii = 0; ii < N_rays; ++ii) {
+		vec3 ray_dir = unit_vector(random_in_hemisphere(orig_rect.normal));
+
+		ray_list.push_back(ray(ray_origin, ray_dir));
+		hit_rec_list.push_back(hit_record());
+		bool ray_hits = tgt_rect.hit(ray_list[ii], 0.0, infinity, hit_rec_list[ii]);
+		
+
+		// if ray hits, it is absorbed (for now assuming totally black surface)
+		if (ray_hits) {
+			hit_count++;
+			gp.sendLine("replot " + ray_list[ii].gnuplot_repr(0,1) + "linecolor 'green'");
+			std::cout << "Ray " + std::to_string(ii) << ": hit\n";
+		}
+		else {
+			gp.sendLine("replot " + ray_list[ii].gnuplot_repr(0, 1) + "linecolor 'red'");
+		}
+	}
+
+	// calculate final view factor (hits / rays
+	double view_factor = hit_count / (double)N_rays;
+	std::cout.precision(3);
+	std::cout << "Estimated View Factor:\t" << std::fixed << view_factor << "(" << hit_count <<
+		"/" << N_rays << ")\n";
+
+	// stop timer
+	auto tend = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double, std::milli> ms_double = tend - tstart;
+	std::cout << "Elapsed Time:\t" << std::fixed << ms_double.count() / 1000.0 << " seconds\n";
+	
+	// finalize the plot
 	gp.sendLine("set mouse");
 	gp.sendLine("set autoscale");
 	gp.sendLine("pause mouse close");
