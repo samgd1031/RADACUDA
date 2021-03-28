cfa4ea938be5e2df993119f7ec3764de78a35389
Sam Sobell <sam.sobell@gmail.com>
2021-03-28 13:00:44 -0400

    use Cmake, fix UB

--- src/material.h
+++ src/material.h
@@ -8,11 +8,13 @@
 
 struct hit_record;
 
+/* this syntax is invalid, but I'm leaving it incase dupas wants to come back to it.
 class material {
 public:
 	virtual bool scatter(
 		const ray& r_in, const hit_record&rec, 
 	);
 };
+*/
 
 #endif // !MATERIAL_H
